a001582b2ef19b998d985431fd88640176ff5719
blingblin-g <luvbliny@gmail.com>
2024-07-10 01:10:02 +0900

    Initial settings

---
+++ src/main/kotlin/site/rookies/guide/GuideApplication.kt
@@ -0,0 +1,11 @@
+package site.rookies.guide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuideApplication
+
+fun main(args: Array<String>) {
+    runApplication<GuideApplication>(*args)
+}
